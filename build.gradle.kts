efe4b4e3e9357ded68efa967eb29f0c541e8bbfd
Christopher Jung <christopher.h.jung@web.de>
2021-07-01 09:14:22 +0200

    use clipper dependancy

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    java
     kotlin("jvm") version "1.4.32"
-    `java-library`
 }
 
 group = "me.chris"
@@ -12,6 +10,7 @@ java.sourceCompatibility = JavaVersion.VERSION_16
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
@@ -19,6 +18,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     //implementation("org.openpnp:opencv:4.5.1-2")
+    implementation("de.lighti:Clipper:6.4.2")
 
     implementation(files("/usr/local/Cellar/opencv/4.5.2_4/share/java/opencv4/opencv-452.jar"))
 }
